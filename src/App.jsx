a6c8fb56108657b37c0e2e840f385248e49ee796
PsammystPrime <samuelprime384@gmail.com>
2024-02-07 00:51:04 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,28 @@ export default function App(){
     <div className="left">
 
       <div className="contact">
+        <h2>Contacts</h2>
         <span>Name:</span>
         <span>Email:</span>
         <span>Phone:</span>  
       </div>
       <div className="education">
+        <h2>Education</h2>
         <span>School:</span>
         <span>Course:</span>
         <span>Date:</span>
       </div>
       <div className="objectives">
+        <h2>Objectives</h2>
         <p>An Innovative and creative techie aspiring to explore the realms of Software Development and Technical Writing</p>
       </div>
     </div>
     
     <div className="right">
       <div className="skills">
+        <h2>Skills</h2>
         <div className="hardSkills">
+          <h4>Technical Skills</h4>
           <ul>
           <li>HTML5</li>
           <li>Vanilla CSS</li>
@@ -36,6 +41,7 @@ export default function App(){
         </ul>
         </div>
         <div className="softSkills">
+          <h4>Soft Skills</h4>
           <ul>
             <li>Problem Solving</li>
             <li>Team Work</li>
@@ -47,12 +53,13 @@ export default function App(){
       </div>
 
       <div className="experience">
+        <h2>Experience</h2>
         <div className="frontend">
-          <h3>Front-End</h3>
+          <h4>Front-End</h4>
           <p>Creating pixel perfect design websites that meet the user's satisfaction and experience. Experienced in working with API's, Es6 Modules, Web Accessibility, Frontend web-Security just to mention but a few.</p>
         </div>
         <div className="writing">
-          <h3>Technical Writing</h3>
+          <h4>Technical Writing</h4>
           <p>Curating Technical Content in the simplest way possible to bring levelty where novice meets professional</p>
         </div>
       </div>
